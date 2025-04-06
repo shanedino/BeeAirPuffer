6854d5f3caef339dcd7cb04073ae2c997f3ee38e
shanedino <shanedino11@gmail.com>
2025-04-06 15:35:48 -0500

    Update SolenoidPERcode.ino

--- SolenoidPERcode/SolenoidPERcode.ino
+++ SolenoidPERcode/SolenoidPERcode.ino
@@ -1,35 +1,31 @@
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
-
-#define LED_PIN 13
-#define BUTTON_PIN 12
-#define MOTOR_SPEED 75
-
-byte ButtonRead = LOW;
+ 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
-Adafruit_DCMotor *myMotor = AFMS.getMotor(3);   
+Adafruit_DCMotor *myMotor = AFMS.getMotor(3);
+ 
+int ButtonValue = 4;  //holds the value received from the button connection
  
 void setup() {
-  Serial.begin(9600);  // this will help with troubleshooting       
-  AFMS.begin(); 
-  pinMode(LED_PIN, OUTPUT); 
-  pinMode(BUTTON_PIN, INPUT); 
-  myMotor->setSpeed(75);  // this only needs to be defined once
+  Serial.begin(9600);  // this will help with troubleshooting          
+  AFMS.begin();  
+  
+myMotor->setSpeed(255);  // this only needs to be defined once
 }
  
 void loop() {
-  ButtonRead = digitalRead(BUTTON_PIN);
+ButtonValue = analogRead(A0);  // read the value from the button connected to pin A0
+ myMotor->run(FORWARD);
+if (ButtonValue > 10000){   // if the button value is above a certain threshold, do something, as per below
+  Serial.print("button pressed: ");
+  Serial.println(ButtonValue);
+  delay(4000);
+  // serial print button value so you know what’s going on
+  Serial.print("Motor running forward");
   myMotor->run(FORWARD);
-  if (ButtonRead == HIGH){  
-    Serial.print("button pressed: ");
-    Serial.println(ButtonRead);
-    delay(4000);
-    Serial.print("Motor running forward");
-    digitalWrite(LED_PIN, HIGH);
-    myMotor->run(FORWARD);
-    delay(4000);
-    Serial.print("Motor stopping");
-    myMotor->run(RELEASE);  // turn off the motor after 4 seconds
-    digitalWrite(LED_PIN, LOW);
-  }
+  delay(4000);
+  Serial.print("Motor stopping");
+  myMotor->run(RELEASE);  // turn off the motor after 4 seconds
+  }  // the if/then loop stops here
+ 
 }
