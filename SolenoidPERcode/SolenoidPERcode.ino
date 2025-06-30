9bb53637e7577dc70f74ada007ea6309eef969f5
zarun7612 <zainuba2@illinois.edu>
2025-06-30 17:27:20 -0500

    Update SolenoidPERcode.ino
    changed blink sequence

--- SolenoidPERcode/SolenoidPERcode.ino
+++ SolenoidPERcode/SolenoidPERcode.ino
@@ -5,30 +5,63 @@
 #define BUTTON_PIN 12
 #define MOTOR_SPEED 75
 
-byte ButtonRead = LOW;
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 Adafruit_DCMotor *myMotor = AFMS.getMotor(3);   
  
+void blinkLED(unsigned long duration, int blinkInterval) {
+  unsigned long startTime = millis();
+  while (millis() - startTime < duration){
+    digitalWrite(LED_PIN, HIGH);
+    delay(blinkInterval);
+    digitalWrite(LED_PIN, LOW);
+    delay(blinkInterval);
+  }
+}
+
 void setup() {
   Serial.begin(9600);  // this will help with troubleshooting       
   AFMS.begin(); 
   pinMode(LED_PIN, OUTPUT); 
   pinMode(BUTTON_PIN, INPUT); 
-  myMotor->setSpeed(110);  // this only needs to be defined once
+  myMotor->setSpeed(MOTOR_SPEED);  // this only needs to be defined once
 }
  
 void loop() {
-  ButtonRead = digitalRead(BUTTON_PIN);
-  if (ButtonRead == LOW){  
-    Serial.print("button pressed: ");
-    Serial.println(ButtonRead);
-    delay(4000);
-    Serial.print("Motor running forward");
-    digitalWrite(LED_PIN, HIGH);
-    myMotor->run(FORWARD);
-    delay(4000);
-    Serial.print("Motor stopping");
-    myMotor->run(RELEASE);  // turn off the motor after 4 seconds
-    digitalWrite(LED_PIN, LOW);
+  for (int i = 0; i < 5; i++) {
+    Serial.print("Cycle ");
+    Serial.println(i + 1);
+  
+  //Blink LED for 20 seconds
+  Serial.println("Blinking LED for 20 seconds (set bee in arena)...");
+  blinkLED(2000, 100);
+
+  //Run motor for 4 seconds air puff to bee face
+  Serial.println("Running airpuff...");
+  digitalWrite(LED_PIN, HIGH);
+  myMotor->run(FORWARD);
+  delay(4000);
+  myMotor->run(RELEASE);
+
+  //Blink LED for 20 seconds
+  Serial.println("Blinking LED for 20 seconds (let bee rest post puff)...");
+  blinkLED(20000, 100);
+
+  //LED solid ON for 5 seconds for loop repeat and bee replacement 
+  Serial.println("LED solid ON for 5 seconds...");
+  digitalWrite(LED_PIN, HIGH);
+  delay(5000);
+  digitalWrite(LED_PIN, LOW);
+
+  delay(1000); //Turn off light so that theres a pause between puff trials
   }
+
+  //Wait for button press to restart loop
+  Serial.println("5 cycles completed. Waiting for button press to restart...");
+  while (digitalRead(BUTTON_PIN) == HIGH) {
+    // Waiting for button press (active LOW)
+    delay(100);
+  }
+  
+  Serial.println("Button pressed. Restarting 5-cycle sequence...");
+  delay(500); //Debounce delay
 }
